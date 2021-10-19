dc9ab0afdb1c803b4cbf7919b93d45243c7e14c4
Adam Graham <adam@zigurous.com>
2021-10-19 08:53:27 -0500

    Fix bug that allows frogger to enter occupied home

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -1,22 +1,31 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Home : MonoBehaviour
 {
     public GameObject frog;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void OnEnable()
     {
         frog.SetActive(true);
+        boxCollider.enabled = false;
     }
 
     private void OnDisable()
     {
         frog.SetActive(false);
+        boxCollider.enabled = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (!enabled && other.tag == "Player")
         {
             enabled = true;
             FindObjectOfType<GameManager>().HomeOccupied();
